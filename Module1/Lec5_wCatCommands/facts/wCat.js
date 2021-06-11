82f282aadbb47d405e55c871ffaf815302f9142f
HarshitChauhan <harshitkumar2503@gmail.com>
2021-06-11 12:48:01 +0530

    wCat commands and acitvity

--- Module1/Lec5_wCatCommands/facts/wCat.js
+++ Module1/Lec5_wCatCommands/facts/wCat.js
@@ -1,43 +1,84 @@
-let fs=require("fs");
-let input=process.argv.slice(2);
+let fs = require("fs");
+let input = process.argv.slice(2);
 
-let files=[];
-let flags=[];
+let files = [];
+let flags = [];
 
-for( let i=0; i< input.length; i++){
-    if(input[i].startsWith("-")){
-        flags.push(input[i]);
-    }else{
-        files.push(input[i]);
-    }
+for (let i = 0; i < input.length; i++) {
+  if (input[i].startsWith("-")) {
+    flags.push(input[i]);
+  } else {
+    files.push(input[i]);
+  }
 }
 
-let data="";
-for( let i=0; i<files.length;i++){
-    let filesKaData=fs.readFileSync(files[i]);
-    data+= i == files.length-1 ? filesKaData : filesKaData+"\n";
+// console.log(files);
+// console.log(flags);
+
+let data = "";
+for(let i=0 ; i<files.length ; i++){
+    let fileKaData = fs.readFileSync(files[i]);
+    data += i == files.length-1 ? fileKaData :  fileKaData+"\r\n";
 }
-//console.log(data);
 
-//-s flag
-function applySflag(){
-    let dataComp=data.split("\r\n");
+// console.log(data);
 
-    let sFlaggedData=[];
+// -s flag
+
+function applySFlag(){
+    let dataComp = data.split("\r\n");
+    // console.log(dataComp);
+    let sFlagedData = [];
+    let nonEmptyFound = false;
     
-    let nonEmptyFound=false;
-
-    for(let i=0;i<dataComp.length;i++){
-        if(dataComp[i] !=''){
-            sFlaggedData.push(dataComp[i]);
-            nonEmptyFound=true;
-        }else if(dataComp[i]=='' && dataComp[i-1] != '' && nonEmptyFound){
-            sFlaggedData.push(dataComp[i]);
+    for(let i=0 ; i<dataComp.length ; i++){
+        if(dataComp[i] != '' ){
+            sFlagedData.push(dataComp[i]);
+            nonEmptyFound = true;
         }
-        
+        else if(dataComp[i] == '' && dataComp[i-1] != '' && nonEmptyFound){
+            sFlagedData.push(dataComp[i]);
+        }
     }
-    let sFlaggedString=sFlaggedData.join("\r\n");
-    return sFlaggedString;
+    let sFlagedString = sFlagedData.join("\r\n");
+    return sFlagedString;
 }
 
-console.log(applySflag());
+// data = applySFlag();
+// console.log(data);
+
+//  -n flag
+function applyNFlag(){
+    let dataComps = data.split("\r\n");
+    let count=1;
+    for(let i=0  ; i<dataComps.length ; i++){
+        // 1.hey i am f1
+        dataComps[i] = `${count}.${dataComps[i]}`;  //string interpolation
+        count++;
+    }
+    // console.log(dataComps);
+    let nFlaggedString = dataComps.join("\r\n");
+    // console.log(nFlaggedString);
+    return nFlaggedString;
+}
+// data = applyNFlag();
+
+
+// -b flag
+
+function applyBFlag(){
+    let dataComps = data.split("\r\n");
+    let count=1;
+    for(let i=0  ; i<dataComps.length ; i++){
+        // 1.hey i am f1
+        if(dataComps[i] != ''){
+            dataComps[i] = `${count}.${dataComps[i]}`;  //string interpolation
+            count++;
+        }
+    }
+    // console.log(dataComps);
+    let bFlaggedString = dataComps.join("\r\n");
+    // console.log(bFlaggedString);
+    return bFlaggedString;
+}
+data = applyBFlag();
