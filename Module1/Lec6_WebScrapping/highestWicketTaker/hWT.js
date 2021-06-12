c9c560123e6737a6c7ebc86ae2445182e3f8fc2a
HarshitChauhan <harshitkumar2503@gmail.com>
2021-06-12 12:30:12 +0530

    hWT activity completed

--- Module1/Lec6_WebScrapping/highestWicketTaker/hWT.js
+++ Module1/Lec6_WebScrapping/highestWicketTaker/hWT.js
@@ -12,6 +12,33 @@ function callback(error, response,html){
 
 function evalHTML(html){
     let ch=cheerio.load(html);
-    let winningTeam=ch('.match-header .status-text span').text();
-    console.log(winningTeam);
+    let allBowlersTrs=ch('.table.bowler tbody tr'); //[of objects]
+    // console.log(allBowlersTrs);
+
+    let highestWicketTaker;
+    let highestWickets;
+    let lowestEconomy;
+    for(let i=0;i<allBowlersTrs.length;i++){
+        let oneBowlerDetail=allBowlersTrs[i];
+        // console.log(oneBowlerDetail);
+        let allTds=ch(oneBowlerDetail).find("td");
+        // console.log(allTds.length);
+        let bowlerName=ch(allTds[0]).text();//index 0
+        let wickets=ch(allTds[4]).text();//index 4
+        let economy=ch(allTds[5]).text();//index 5
+    
+        if(i==0){
+            highestWicketTaker=bowlerName;
+            highestWickets=wickets;
+            lowestEconomy=economy;
+        }else{
+            if(wickets>highestWickets || (wickets==highestWickets && economy<lowestEconomy)){
+                highestWicketTaker=bowlerName;
+                highestWickets=wickets;
+                lowestEconomy=economy;
+            }
+        }
+    }
+    console.log(`The highest wicket taker is:\n Name:\t\t${highestWicketTaker}\n Wickets:\t${highestWickets}\n Economy:\t${lowestEconomy}`);
+
 }
